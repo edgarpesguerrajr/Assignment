9a4c694fb42a661204d840078a963f5b8a86870c
Edgar Esguerra Jr <92613205+edgarpesguerrajr@users.noreply.github.com>
2021-10-23 17:58:41 +0800

    Delete 02.3.py

--- 02.3.py
+++
@@ -1,7 +0,0 @@
-money_str = input('Enter the amount of your money: ')
-apples_str = input('Enter the price of the apple: ')
-money = int(money_str)
-apple = int(apples_str)
-totalapple = money//apple
-totalamount = totalapple*apple
-print(f'You can buy {totalapple} apples and your change is {money-totalamount} pesos.')
